7e64068fbf788d1d805df31ccfb4a32d0da54391
muqingcloud <lyonmu@foxmail.com>
2022-04-11 17:29:09 +0800

    use Ribbon RandomRule

--- system-consumer/src/main/java/space/muqingcloud/MainConsumerOperation.java
+++ system-consumer/src/main/java/space/muqingcloud/MainConsumerOperation.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import space.myrule.MySelfRule;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient // 服务发现与注册注解
+@RibbonClient(name = "SYSTEM-USER", configuration = MySelfRule.class)
 public class MainConsumerOperation {
     public static void main(String[] args) {
         SpringApplication.run(MainConsumerOperation.class, args);
