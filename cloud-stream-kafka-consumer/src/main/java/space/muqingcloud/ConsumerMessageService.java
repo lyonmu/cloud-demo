547a6ea5ed698ea195f32e9fe56b6f4c3c412efa
muqingcloud <lyonmu@foxmail.com>
2022-04-13 17:37:24 +0800

    use Spring Cloud Stream consumer message to kafka

---
+++ cloud-stream-kafka-consumer/src/main/java/space/muqingcloud/ConsumerMessageService.java
@@ -0,0 +1,21 @@
+package space.muqingcloud;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Service;
+
+@Service
+@EnableBinding(Sink.class)
+public class ConsumerMessageService {
+    @Value("${server.port}")
+    private String serverPort;
+
+
+    @StreamListener(Sink.INPUT)
+    public void input(Message<String> message) {
+        System.out.println("消费者:" + serverPort + ",----->接受到的消息: " + message.getPayload() + "\t  port: " + serverPort);
+    }
+}
