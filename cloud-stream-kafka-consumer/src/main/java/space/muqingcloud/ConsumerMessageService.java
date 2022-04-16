ee7d2f4a7cb7b7b9c3557ce818ba3ea14f7690b0
muqingcloud <lyonmu@foxmail.com>
2022-04-16 20:22:14 +0800

    optimize Spring Cloud Stream for kafka

--- cloud-stream-kafka-consumer/src/main/java/space/muqingcloud/ConsumerMessageService.java
+++ cloud-stream-kafka-consumer/src/main/java/space/muqingcloud/ConsumerMessageService.java
@@ -1,5 +1,7 @@
 package space.muqingcloud;
 
+import com.google.gson.Gson;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @EnableBinding(Sink.class)
+@Slf4j
 public class ConsumerMessageService {
     @Value("${server.port}")
     private String serverPort;
@@ -16,6 +19,8 @@ public class ConsumerMessageService {
 
     @StreamListener(Sink.INPUT)
     public void input(Message<String> message) {
+
+        log.info(message.getPayload());
         System.out.println("消费者:" + serverPort + ",----->接受到的消息: " + message.getPayload() + "\t  port: " + serverPort);
     }
 }
